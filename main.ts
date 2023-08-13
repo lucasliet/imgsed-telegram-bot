94749cfed2f71227cc20e60fd98e94ce6f827f7b
lucasliet <lucasouliveira@gmail.com>
2023-08-13 18:56:48 -0300

    Override Deno.readAll to fix telegram bot lib

--- main.ts
+++ main.ts
@@ -1,10 +1,17 @@
 import * as Telegram from 'https://deno.land/x/telegram@v0.1.1/mod.ts';
+import { readAll } from 'https://deno.land/std@0.198.0/streams/read_all.ts';
 import { replyMediaContent } from './service.ts';
 
 const TOKEN: string = Deno.env.get('BOT_TOKEN') as string;
 const PORT: number = parseInt(Deno.env.get('PORT') as string) || 80;
 
 const BOT = new Telegram.Bot(TOKEN);
+
+// Override Deno.readAll to fix Telegram Bot API
+Deno.readAll = async (r: any) => {
+  console.info('Override deprecated Deno.readAll')
+  return await readAll(r);
+};
 
 BOT.on('text', async (ctx) => {
   const link: string | undefined = ctx.message?.text;
