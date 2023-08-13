bf16ad632288a4a6facbebf2c0b98aff20c7c7e1
Lucas Oliveira <lucasouliveira@gmail.com>
2023-08-12 23:39:57 -0300

    use deno.land telegram dependency

--- main.ts
+++ main.ts
@@ -1,31 +1,25 @@
-import { Telegraf } from 'npm:telegraf';
-import { DOMParser, Element, Node } from 'https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts';
+import * as Telegram from 'https://deno.land/x/telegram@v0.1.1/mod.ts';
+import { DOMParser, Element } from 'https://deno.land/x/deno_dom@v0.1.36-alpha/deno-dom-wasm.ts';
 
-const BOT = new Telegraf(Deno.env.get('BOT_TOKEN'));
+const BOT = new Telegram.Bot(Deno.env.get('BOT_TOKEN') as string);
 const DOM_PARSER = new DOMParser();
 
-BOT.start((ctx) => ctx.reply('Welcome!, Send me a instagram reel/picture link and I will download it for you'));
-
 BOT.on('message', async (ctx) => {
-  const link: string = ctx.message.text;
-  if (link.includes('instagram.com/reel')) {
-    const reel = await convertTelegramLink(link);
-    ctx.replyWithVideo({ url: reel });
-  } else if (link.includes('instagram.com/p')) {
-    const picture = await convertTelegramLink(link);
-    ctx.replyWithPhoto({ url: picture });
+  const link: string = ctx.message!.text!;
+  if (link.includes('instagram.com/reel') || link.includes('instagram.com/p')) {
+    const mediaLink: string = await convertTelegramLink(link);
+    ctx.reply(mediaLink);
   } else {
     ctx.reply('Please send me a instagram reel/picture link');
   }
 });
 
-async function convertTelegramLink(url:string) {
+async function convertTelegramLink(url:string): Promise<string> {
     const mediaId = url.split('instagram.com')[1].split('/')[2];
     const response = 
       await fetch(`https://imgsed.com/p/${mediaId}`).then((res) => res.text());
 
-    const mediaLink = parseElement(response, '.media-wrap').getAttribute('data-video');
-    return mediaLink;
+    return parseElement(response, '.media-wrap').getAttribute('data-video')!;
 }
 
 function parseElement(html: string, selector: string): Element {
